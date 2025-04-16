ce5eb029b7c81465cc72041a11983cfde3657bf6
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2025-04-16 11:32:10 +0200

    Updated store to use Local Storage

--- stores/tasks.ts
+++ stores/tasks.ts
@@ -1,19 +1,39 @@
+// stores/tasks.ts
 import { defineStore } from 'pinia';
+import { useStorage } from '@vueuse/core';
 
-export const useTaskStore = defineStore('task', {
-  state: () => ({
-    tasks: [
-      { id: 1, title: 'First task', done: false },
-      { id: 2, title: 'Second task', done: true },
-    ],
-  }),
-  actions: {
-    toggleTask(id: number) {
-      const task = this.tasks.find((t) => t.id === id);
-      if (task) task.done = !task.done;
-    },
-    reorderTasks(newOrder: any[]) {
-      this.tasks = newOrder;
-    },
-  },
+export const useTaskStore = defineStore('task', () => {
+  const tasks = useStorage('taskify-tasks', [
+    { id: 1, title: 'First task', done: false },
+    { id: 2, title: 'Second task', done: true },
+  ]);
+
+  function toggleTask(id: number) {
+    const task = tasks.value.find((t) => t.id === id);
+    if (task) task.done = !task.done;
+  }
+
+  function reorderTasks(newOrder: any[]) {
+    tasks.value = newOrder;
+  }
+
+  function addTask(title: string) {
+    tasks.value.push({
+      id: Date.now(),
+      title,
+      done: false,
+    });
+  }
+
+  function deleteTask(id: number) {
+    tasks.value = tasks.value.filter((t) => t.id !== id);
+  }
+
+  return {
+    tasks,
+    toggleTask,
+    reorderTasks,
+    addTask,
+    deleteTask,
+  };
 });
